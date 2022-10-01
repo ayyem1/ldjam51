1e49a28daf3485aa53ec5c68468504d9b3bc49e4
ayyem1 <ayyem@proton.me>
2022-10-01 14:51:34 -0400

    Created card prefab and hooked up deck builder dialog.

--- Assets/Scripts/MapSceneUIController.cs
+++ Assets/Scripts/MapSceneUIController.cs
@@ -1,14 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapSceneUIController : MonoBehaviour
 {
     public TMP_Text HealthLabel;
     public TMP_Text CorporateBucksLabel;
 
+    public GameObject DeckBuilderDialog;
     private void Start()
     {
         HealthLabel.text = GameInstance.Instance.MainPlayer.HpDisplayText;
         CorporateBucksLabel.text = GameInstance.Instance.MainPlayer.CurrentCorporateBucksAmount.ToString();
+    }
+
+    public void OnPressDeck()
+    {
+        DeckBuilderDialog.gameObject.SetActive(true);
     }
 }
