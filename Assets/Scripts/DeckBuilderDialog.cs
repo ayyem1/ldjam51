390e511a6afbe79dc81a862d77a4d595a24348ce
ayyem1 <ayyem@proton.me>
2022-10-01 18:11:40 -0400

    Initialized card data in deck builder script.

--- Assets/Scripts/DeckBuilderDialog.cs
+++ Assets/Scripts/DeckBuilderDialog.cs
@@ -2,6 +2,33 @@ using UnityEngine;
 
 public class DeckBuilderDialog : MonoBehaviour
 {
+    [SerializeField] private CardUI[] CardsInDeck;
+    [SerializeField] private CardUI[] CardsInReserve;
+
+    private void Start()
+    {
+        var playerCards = GameInstance.Instance.MainPlayer.CardsInDeck;
+        var i = 0;
+        for (; i < playerCards.Count; i++)
+        {
+            CardsInDeck[i].Initialize(playerCards[i]);
+        }
+        for (; i < CardsInDeck.Length; i++)
+        {
+            CardsInDeck[i].Initialize(null);
+        }
+
+        var reserveCards = GameInstance.Instance.MainPlayer.ReserveCards;
+        i = 0;
+        for (; i < reserveCards.Count; i++)
+        {
+            CardsInReserve[i].Initialize(reserveCards[i]);
+        }
+        for (; i < CardsInReserve.Length; i++)
+        {
+            CardsInReserve[i].Initialize(null);
+        }
+    }
     public void OnBackButtonPressed()
     {
         gameObject.SetActive(false);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
     public float CurrentHp { get; set; }
     public string HpDisplayText { get { return $"{CurrentHp}/{maxHp}"; } }
     public int CurrentCorporateBucksAmount { get; set; }
-    public List<Card> OwnedCards { get; set; } = new List<Card>();
+    public List<Card> ReserveCards { get; set; } = new List<Card>();
     public List<Card> CardsInDeck { get; set; } = new List<Card>();
 
     public void InitializePlayer()
@@ -20,7 +20,6 @@ public class Player : MonoBehaviour
         Name = "New Guy"; // TODO: Ask user for name.
         CurrentHp = startingHp;
         CurrentCorporateBucksAmount = startingCorporateBucksAmount;
-        OwnedCards.AddRange(startingCards);
         CardsInDeck.AddRange(startingCards);
     }
 }
